055ff958795cc24b4f8a0ca60ff615cd98df5f6c
root <wj87917@hotmail.com>
2016-09-02 16:10:09 +0800

    change log

--- sys/nfp/60_output.py
+++ sys/nfp/60_output.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 output = """
-[{"endpoint": "host01", "tags": "", "timestamp": 1431349763, "metric": "sys.ntp.offset", "value": 0.73699999999999999, "counterType": "GAUGE", "step": 600}]
+[{"endpoint": "J-PC", "tags": "", "timestamp": 1431349763, "metric": "sys.ntp.offset", "value": 0.73699999999999999, "counterType": "GAUGE", "step": 600}]
 """
 
 if __name__ == "__main__":
